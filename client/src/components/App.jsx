7f63f57830b19bc229004b39b90cfc20fbaa252c
fabricio-a <fabricio.santana402@outlook.com>
2020-12-03 09:14:59 -0300

    criação dos componentes basicos

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/App.css'
+
+import Cadastro from './Cadastro'
+import Mapa from './Mapa'
+
+export default () =>
+    <React.Fragment>
+        <Cadastro />
+        <Mapa />
+    </React.Fragment>
